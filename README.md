f98115be53509dc8f4195eda9223652c09b0a98a
B-Schwarz <29834059+B-Schwarz@users.noreply.github.com>
2021-12-18 13:59:01 +0100

    Update Readme

--- README.md
+++ README.md
@@ -2,10 +2,23 @@
 
 Designed to assist in playing the game
 
+### Table of Contens
+- [Character editor](#character-editor)
+- [Combat tracker](#combat-tracker)
+- [Encounter editor](#encounter-editor)
+- [Setting](#settings)
+- [Admin](#admin)
+
+---
+
 ## Character editor
 
+[X] List all of my character sheets  
+[X] List all character sheets by other players (admin & master only)  
 [X] Every user is able to create and modify characters. The character sheet is saved 
 everytime they're changed.
+
+---
 
 ## Combat tracker
 
@@ -22,11 +35,26 @@ everytime they're changed.
 [ ] Button to mark character's turn  
 [ ] Dungeon Master can hide characters from players 
 
+---
+
 ## Encounter editor
 
 [ ] Create custom encounters with saved enemies for quickly loading them
 
+---
+
 ## Settings
 
 [ ] A user should be able to change their password  
-[ ] A user should be able to delete their account + all characters
+[X] A user should be able to delete their account + all characters
+
+---
+
+## Admin 
+
+[ ] Export character sheets  
+[ ] Import character sheets  
+[ ] Duplicate character sheets  
+[ ] Reassign character sheets to other users (only one at a time)  
+[ ] Delete another user account  
+[ ] Change another user's password  
--- api/settings/index.js
+++ api/settings/index.js
@@ -1,2 +1,59 @@
-const { User } = require('../db/models/user.model')
+const {User} = require('../db/models/user.model')
+const {Character} = require('../db/models/character.model')
 
+const deleteOwnAccount = async (req, res) => {
+    const chars = req.user.character
+
+    if (chars.length > 0) {
+        await Character.deleteMany({
+            _id: {
+                $in: chars
+            }
+        }, () => {
+        }).catch(() => {
+        })
+    }
+
+
+    await User.deleteOne({_id: req.user._id}, () => {
+    }).catch(() => {})
+    req.session.destroy()
+
+    res.sendStatus(200)
+}
+
+// REQUIRES ADMIN
+const deleteAccount = async (req, res) => {
+    const userID = req.body.userID
+
+    if (userID) {
+        try {
+            const user = await User.findOne({_id: userID})
+
+            const chars = user.character
+
+            if (chars.length > 0) {
+                await Character.deleteMany({
+                    _id: {
+                        $in: chars
+                    }
+                }, () => {
+                }).catch(() => {})
+            }
+
+            await User.deleteOne({_id: userID}, () => {
+            }).catch(() => {})
+
+            res.sendStatus(200)
+        } catch (_) {
+            res.sendStatus(500)
+        }
+    } else {
+        res.sendStatus(400)
+    }
+}
+
+module.exports = {
+    deleteOwnAccount,
+    deleteAccount
+}
