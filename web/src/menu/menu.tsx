41cde50a954dbc4608058e14e185874aee507cf8
B-Schwarz <29834059+B-Schwarz@users.noreply.github.com>
2021-12-18 10:39:29 +0100

    Menu Update

--- web/src/menu/menu.tsx
+++ web/src/menu/menu.tsx
@@ -1,8 +1,9 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {Box, Button} from "@chakra-ui/react";
 import {SelectedEnum} from "./selected.enum";
 import {Divider} from "@chakra-ui/layout";
 import {useNavigate} from "react-router-dom";
+import {SettingsIcon} from "@chakra-ui/icons";
 
 const Menu = (props: { selected: SelectedEnum; }) => {
 
@@ -13,6 +14,11 @@ const Menu = (props: { selected: SelectedEnum; }) => {
         }, {
             name: 'Initiative',
             selected: SelectedEnum.INITIATIVE
+        }, {
+            name: 'Settings',
+            icon: <SettingsIcon/>,
+            color: 'teal',
+            selected: SelectedEnum.SETTINGS
         }]
 
     const navigate = useNavigate()
@@ -22,7 +28,7 @@ const Menu = (props: { selected: SelectedEnum; }) => {
             <Box padding='0.2rem'>
                 {
                     btn.map((b) => (
-                        <Button key={b.selected} colorScheme='gray'
+                        <Button key={b.selected} colorScheme={b.color || 'gray'} leftIcon={b.icon || <></>}
                                 variant={props.selected === b.selected ? 'solid' : 'ghost'}
                                 onClick={() => {
                                     navigate(`/${b.name.toLowerCase()}`)
@@ -32,7 +38,7 @@ const Menu = (props: { selected: SelectedEnum; }) => {
                     ))
                 }
             </Box>
-            <Divider/>
+            <Divider marginBottom='1rem'/>
         </>
     )
 
--- web/src/menu/selected.enum.ts
+++ web/src/menu/selected.enum.ts
@@ -1,5 +1,6 @@
 export enum SelectedEnum {
     NONE,
     CHARACTER,
-    INITIATIVE
+    INITIATIVE,
+    SETTINGS
 }
