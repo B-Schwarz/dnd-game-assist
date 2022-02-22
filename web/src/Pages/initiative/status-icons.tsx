389f72787696530f1efbb4b2338a74160e7368e8
B-Schwarz <29834059+B-Schwarz@users.noreply.github.com>
2022-02-22 01:38:47 +0100

    Version: 1.4.0, Add tooltip for icons

--- web/src/Pages/initiative/status-icons.tsx
+++ web/src/Pages/initiative/status-icons.tsx
@@ -1,5 +1,5 @@
 import {StatusEffectsEnum} from "./status-effects.enum";
-import {Box, Icon} from "@chakra-ui/react";
+import {Box, Icon, Tooltip} from "@chakra-ui/react";
 import {IoArrowDownSharp, IoEarOutline, IoEyeOffSharp, IoWaterSharp} from "react-icons/io5";
 import React from "react";
 import {
@@ -16,100 +16,156 @@ import {
 } from "react-icons/gi";
 
 const prone = (
-    <React.Fragment key='down'>
+    <React.Fragment key='prone'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiBootStomp} color='red'/>
+        <Tooltip label='Liegend' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiBootStomp} color='red'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const blind = (
     <React.Fragment key='blind'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={IoEyeOffSharp} color='purple'/>
+        <Tooltip label='Blind' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={IoEyeOffSharp} color='purple'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const poison = (
     <React.Fragment key='poison'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={IoWaterSharp} color='green'/>
+        <Tooltip label='Vergifted' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={IoWaterSharp} color='green'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const charmed = (
     <React.Fragment key='charmed'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiCharm} color="#ff3dda"/>
+        <Tooltip label='Bezaubert' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiCharm} color="#ff3dda"/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const deafened = (
     <React.Fragment key='deafened'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={IoEarOutline} color='purple'/>
+        <Tooltip label='Taub' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={IoEarOutline} color='purple'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const frightened = (
     <React.Fragment key='frightened'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiChicken} color='brown'/>
+        <Tooltip label='Verängstigt' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiChicken} color='brown'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const grappled = (
     <React.Fragment key='grappled'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiHook} color='purple'/>
+        <Tooltip label='Gepackt' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiHook} color='purple'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const incapacitated = (
     <React.Fragment key='incapacitated'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={IoArrowDownSharp} color='purple'/>
+        <Tooltip label='Kampfunfähig' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={IoArrowDownSharp} color='purple'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const invisible = (
     <React.Fragment key='invisible'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiInvisible} color='blue'/>
+        <Tooltip label='Unsichtbar' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiInvisible} color='blue'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const paralyzed = (
     <React.Fragment key='paralyzed'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiThunderStruck} color='orange'/>
+        <Tooltip label='Gelähmt' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiThunderStruck} color='orange'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const petrified = (
     <React.Fragment key='petrified'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiStoneBlock} color='gray'/>
+        <Tooltip label='Versteinert' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiStoneBlock} color='gray'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const restrained = (
     <React.Fragment key='restrained'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiSpiderWeb} color='purple'/>
+        <Tooltip label='Festgesetzt' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiSpiderWeb} color='purple'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const stunned = (
     <React.Fragment key='stunned'>
         <Box marginLeft='0.5rem'/>
-        <Icon as={GiKnockedOutStars} color='orange'/>
+        <Tooltip label='Betäubt' hasArrow size='md' placement='top'>
+            <span>
+                <Icon as={GiKnockedOutStars} color='orange'/>
+            </span>
+        </Tooltip>
     </React.Fragment>
 )
 
 const unconscious = (
     <React.Fragment key='unconscious'>
         <Box marginLeft='0.5rem'/>
+        <Tooltip label='Bewusstlos' hasArrow size='md' placement='top'>
+        <span>
         <Icon as={GiKnockout} color='darkred'/>
+                </span>
+        </Tooltip>
     </React.Fragment>
 )
 
