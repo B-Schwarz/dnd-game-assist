309116ea58ba0cc452c6d971447f936a8c3c4ee7
B-Schwarz <29834059+B-Schwarz@users.noreply.github.com>
2022-03-07 15:44:03 +0100

    Added pages to monster list

--- web/src/Pages/monster/monster-list.tsx
+++ web/src/Pages/monster/monster-list.tsx
@@ -2,11 +2,13 @@ import React, {useEffect, useState} from "react";
 import WithAuth from "../login/withAuth";
 import {Monster} from "./monster.type";
 import MonsterEntry from "./monster-entry";
-import {Accordion, HStack, IconButton, Input, VStack} from "@chakra-ui/react";
-import _ from "lodash";
+import {Accordion, Button, HStack, IconButton, Input, StackItem, VStack} from "@chakra-ui/react";
 import axios from "axios";
 import {AddIcon} from "@chakra-ui/icons";
 import TitleService from "../../Service/titleService";
+import {Text} from "@chakra-ui/layout";
+import {AiOutlineArrowLeft} from "@react-icons/all-files/ai/AiOutlineArrowLeft";
+import {AiOutlineArrowRight} from "@react-icons/all-files/ai/AiOutlineArrowRight";
 
 const App = () => {
 
@@ -14,35 +16,33 @@ const App = () => {
     const [value, setValue] = useState<Monster[]>([])
     const [isAdminOrMaster, setIsAdminOrMaster] = useState(false)
 
-    useEffect(() => {
-        update()
-        axios.get(process.env.REACT_APP_API_PREFIX + '/api/me/admin/master')
-            .then(() => setIsAdminOrMaster(true))
-            .catch(() => {
-            })
-    }, [])
+    const [page, setPage] = useState(1)
 
-    useEffect(() => {
-        setValue(monster)
-    }, [monster, isAdminOrMaster])
+    const [disableLeft, setDisableLeft] = useState(true)
+    const [disableRight, setDisableRight] = useState(false)
+
+    const items = 30;
 
     const update = () => {
         axios.get(process.env.REACT_APP_API_PREFIX + '/api/monster/list')
             .then((data) => {
                 setMonster([])
                 setMonster(data.data)
+                switchPage(data.data, 1)
+            })
+            .finally(() => {
+                axios.get(process.env.REACT_APP_API_PREFIX + '/api/me/admin/master')
+                    .then(() => setIsAdminOrMaster(true))
+                    .catch(() => {
+                    })
             })
     }
 
     const search = (val: string) => {
         if (val.length > 0) {
-            const temp = _.cloneDeep(value)
-            temp.forEach(d => d.hidden = !d.monster.name.toLowerCase().startsWith(val.toLowerCase()))
-            setValue([])
-            setValue(temp)
+            switchPage(monster.filter(d => d.monster.name.toLowerCase().startsWith(val.toLowerCase())), 1)
         } else {
-            setValue([])
-            setValue(monster)
+            switchPage(monster, 1)
         }
     }
 
@@ -52,6 +52,32 @@ const App = () => {
             .catch(() => {
             })
     }
+
+    const switchPage = (data: Monster[], pPage: number) => {
+        if (pPage > 0) {
+            const temp = data.slice(items * (pPage - 1), items * pPage)
+            if (temp.length > 0) {
+                setValue(temp)
+                setPage(pPage)
+            }
+
+            if (pPage > 1) {
+                setDisableLeft(false)
+            } else {
+                setDisableLeft(true)
+            }
+
+            if (data[items * pPage] === undefined) {
+                setDisableRight(true)
+            } else {
+                setDisableRight(false)
+            }
+        }
+    }
+
+    useEffect(() => {
+        update()
+    }, [update])
 
     return (
         <React.Fragment>
@@ -65,14 +91,23 @@ const App = () => {
                                                     onClick={createMonster}/>}
                 </HStack>
                 <Accordion allowToggle w='100%' h='80vh' overflowY='scroll'>
-                    {value.map((m: Monster, i: number) => {
+                    {value.map((m: Monster) => {
                             if (!m.hidden)
-                                 return (  <MonsterEntry m={m} u={update} e={isAdminOrMaster} key={i}/> )
+                                return (<MonsterEntry m={m} u={update} e={isAdminOrMaster} key={m._id}/>)
                             else
-                                return <React.Fragment key={i}/>
-                    }
+                                return <React.Fragment key={m._id}/>
+                        }
                     )}
                 </Accordion>
+                <StackItem>
+                    <HStack spacing='1rem'>
+                        <Button disabled={disableLeft}
+                                onClick={() => switchPage(monster, page - 1)}><AiOutlineArrowLeft/></Button>
+                        <Text textAlign='center' width='5rem'>{page}</Text>
+                        <Button disabled={disableRight}
+                                onClick={() => switchPage(monster, page + 1)}><AiOutlineArrowRight/></Button>
+                    </HStack>
+                </StackItem>
             </VStack>
         </React.Fragment>
     )
